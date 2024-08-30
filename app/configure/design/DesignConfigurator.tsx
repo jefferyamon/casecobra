c3a3f1a818c11b3dcf3f3baaf5b757d7b6d970c9
unknown <jefferyamon2468@gmail.com>
2024-08-30 17:35:56 +0100

    update

--- app/configure/design/DesignConfigurator.tsx
+++ app/configure/design/DesignConfigurator.tsx
@@ -42,7 +42,7 @@ const DesignConfigurator = ({
 }: DesignConfiguratorProps) => {
   const { toast } = useToast();
   const router = useRouter();
-  const { mutate: saveConfig } = useMutation({
+  const { mutate: saveConfig, isPending } = useMutation({
     mutationKey: ["save-config"],
     mutationFn: async (args: SaveConfigArgs) => {
       await Promise.all([saveConfiguration(), _saveConfig(args)]);
@@ -378,6 +378,9 @@ const DesignConfigurator = ({
                 )}
               </p>
               <Button
+                isLoading={isPending}
+                disabled={isPending}
+                loadingText="Saving"
                 size="sm"
                 className="w-full"
                 onClick={() =>
